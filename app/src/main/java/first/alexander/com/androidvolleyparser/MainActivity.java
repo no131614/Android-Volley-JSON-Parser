f138330aaa61a1a4ec6bc134acde9e5bb5e21f4a
Alexander Julianto <alexander.julianto94@gmail.com>
2017-08-17 18:32:06 -0700

    Added check for cancelled order

--- app/src/main/java/first/alexander/com/androidvolleyparser/MainActivity.java
+++ app/src/main/java/first/alexander/com/androidvolleyparser/MainActivity.java
@@ -79,30 +79,34 @@ public class MainActivity extends AppCompatActivity {
                             //Reset Amount
                             bronze_bag_count = 0;
 
-                            //Get the Order JSON Array
+                            // Get the Order JSON Array
                             JSONArray OrderArray = response.getJSONArray("orders");
 
-                            //Tracing trough the Order array
+                            // Tracing trough the Order array
                             for (int order_index = 0; order_index < OrderArray.length(); order_index++) {
 
-                                //Get an order
+                                // Get an order
                                 JSONObject Order = OrderArray.getJSONObject(order_index);
 
-                                //Get a line items array from an order
-                                JSONArray line_itemsArray = Order.getJSONArray("line_items");
+                                // Make sure order is not cancelled
+                                if (Order.getString("cancel_reason").equals("null")) {
 
+                                    // Get a line items array from an order
+                                    JSONArray line_itemsArray = Order.getJSONArray("line_items");
 
-                                //Tracing trough the line items array
-                                for (int line_index = 0; line_index < line_itemsArray.length(); line_index++) {
+                                    // Tracing trough the line items array
+                                    for (int line_index = 0; line_index < line_itemsArray.length(); line_index++) {
 
-                                    //Get a line item
-                                    JSONObject Item = line_itemsArray.getJSONObject(line_index);
+                                        // Get a line item
+                                        JSONObject Item = line_itemsArray.getJSONObject(line_index);
 
-                                    //Get the item title
-                                    String item_title = Item.getString("title");
+                                        // Get the item title
+                                        String item_title = Item.getString("title");
 
-                                    if (item_title.equals(ITEM_NAME)) {
-                                        bronze_bag_count += Item.getInt("quantity");
+                                        if (item_title.equals(ITEM_NAME)) {
+                                            bronze_bag_count += Item.getInt("quantity");
+                                        }
+
                                     }
 
                                 }
@@ -142,37 +146,42 @@ public class MainActivity extends AppCompatActivity {
 
                         try {
 
-                            //Reset Amount
+                            // Reset Amount
                             total_price_amount = 0;
 
-                            //Get the Order JSON Array
+                            // Get the Order JSON Array
                             JSONArray OrderArray = response.getJSONArray("orders");
 
-                            //Tracing trough the Order array
+                            // Tracing trough the Order array
                             for (int order_index = 0; order_index < OrderArray.length(); order_index++) {
 
-                                //Get an order
+                                // Get an order
                                 JSONObject Order = OrderArray.getJSONObject(order_index);
 
-                                //Get a line items array from an order
-                                JSONArray line_itemsArray = Order.getJSONArray("line_items");
+                                // Make sure order is not cancelled
+                                if (Order.getString("cancel_reason").equals("null")) {
 
-                                //Get customer info
-                                try {
-                                    JSONObject Customer = Order.getJSONObject("customer");// Customer might not exist
+                                    // Get a line items array from an order
+                                    JSONArray line_itemsArray = Order.getJSONArray("line_items");
 
-                                    // Begin: Check if it is Napoleon Batz and calculate his total price
-                                    String first_name = Customer.getString("first_name");
-                                    String last_name = Customer.getString("last_name");
-                                    if (first_name.equals(FIRST_NAME) && last_name.equals(LAST_NAME)) {
+                                    // Get customer info
+                                    try {
+                                        JSONObject Customer = Order.getJSONObject("customer");// Customer might not exist
 
-                                        //Get total price for an order
-                                        total_price_amount += Order.getDouble("total_price");
+                                        // Begin: Check if it is Napoleon Batz and calculate his total price
+                                        String first_name = Customer.getString("first_name");
+                                        String last_name = Customer.getString("last_name");
+                                        if (first_name.equals(FIRST_NAME) && last_name.equals(LAST_NAME)) {
 
+                                            //Get total price for an order
+                                            total_price_amount += Order.getDouble("total_price");
+
+                                        }
+                                        // End: Check if it is Napoleon Batz and calculate his total price
+                                    } catch (JSONException JE) {
+                                        name_number = Order.getString("name");
                                     }
-                                    // End: Check if it is Napoleon Batz and calculate his total price
-                                } catch (JSONException JE) {
-                                    name_number = Order.getString("name");
+
                                 }
 
                             }
